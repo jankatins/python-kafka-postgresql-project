e34c40b6d2655689b7c0ff206ebdb963d75b47d0
Jan Katins <jasc@gmx.net>
2020-11-28 21:21:21 +0100

    Add simple runtime validation to the event

--- src/checkweb/check_event.py
+++ src/checkweb/check_event.py
@@ -17,6 +17,15 @@ class CheckEvent():
     exception_message: t.Optional[str] = None
     version: int = 1
 
+    def __post_init__(self):
+        # some validation, should probably raise a better exception
+        if self.exception_message:
+            msg = "If exception_message is set, status_code and found_regex_pattern must both be None"
+            assert self.status_code is None and self.found_regex_pattern is None, msg
+        assert self.response_time_seconds > 0, "response_time_seconds must be greater than 0"
+        assert self.status_code is None or self.status_code > 0, "status_code must be None or an integer >0"
+        assert self.url, "url must be set to a non-empty string"
+
     def to_dict(self):
         """Converts this CheckEvent instance to a dict"""
         return dataclasses.asdict(self)
